9b441ff80ff20e6d7a18fffb004c46fbe497a932
fleibetseder <leibi.fl@gmail.com>
2019-11-14 18:23:51 +0100

    Best Practice

--- CashDesk.Tests/TestDeleteMember.cs
+++ CashDesk.Tests/TestDeleteMember.cs
@@ -9,33 +9,33 @@ namespace CashDesk.Tests
         [Fact]
         public void InvalidParameters()
         {
-            using (var ab = new DataAccess())
+            using (var dal = new DataAccess())
             {
-                Assert.ThrowsAsync<ArgumentException>(async () => await ab.DeleteMemberAsync(Int32.MaxValue));
+                Assert.ThrowsAsync<ArgumentException>(async () => await dal.DeleteMemberAsync(Int32.MaxValue));
             }
         }
 
         [Fact]
         public async Task DeleteMember()
         {
-            using (var ab = new DataAccess())
+            using (var dal = new DataAccess())
             {
-                await ab.InitializeDatabaseAsync();
-                var mnummer = await ab.AddMemberAsync("Foo", "DeleteMember", DateTime.Today.AddYears(-18));
-                await ab.DeleteMemberAsync(mnummer);
+                await dal.InitializeDatabaseAsync();
+                var memberNumber = await dal.AddMemberAsync("Foo", "DeleteMember", DateTime.Today.AddYears(-18));
+                await dal.DeleteMemberAsync(memberNumber);
             }
         }
 
         [Fact]
         public async Task CascadeDeleteMember()
         {
-            using (var ab = new DataAccess())
+            using (var dal = new DataAccess())
             {
-                await ab.InitializeDatabaseAsync();
-                var mnummer = await ab.AddMemberAsync("Foo", "CascadeDeleteMember", DateTime.Today.AddYears(-18));
-                await ab.JoinMemberAsync(mnummer);
-                await ab.DepositAsync(mnummer, 100);
-                await ab.DeleteMemberAsync(mnummer);
+                await dal.InitializeDatabaseAsync();
+                var memberNumber = await dal.AddMemberAsync("Foo", "CascadeDeleteMember", DateTime.Today.AddYears(-18));
+                await dal.JoinMemberAsync(memberNumber);
+                await dal.DepositAsync(memberNumber, 100);
+                await dal.DeleteMemberAsync(memberNumber);
             }
         }
     }
